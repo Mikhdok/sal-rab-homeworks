b74e73e4997cf735696b3478945fef755a23c03f
misha <dokuchaev_05@mail.ru>
2021-05-22 20:00:55 +0300

    1.1

--- task_4/solution.js
+++ task_4/solution.js
@@ -2,17 +2,25 @@
 
 function parseProducts(json) {
     // Аргументом функции является JSON
-
-    // Преобразуйте строку json, переданную как аргумент функции,
-    // в объект с помощью функции JSON.parse(json)
-    // и запишите в переменную data
+ 
+     let data = JSON.parse(json)// Преобразуйте строку json, переданную как аргумент функции,
+     let products = data.products // в объект с помощью функции JSON.parse(json)
+     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
-}
-
-// Напишите функцию renderProductsCards(json)
-// Аргументом функции является JSON
-// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-// Запишите в переменную length значение свойства products.length
-// Напишите цикл, в котором перебираете все products от 0 до (length)
-// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+ return data.products;
+ }
+ 
+ function renderProductsCards(json){
+     clearProducts(); 
+ 
+   let products = parseProducts(json) // Напишите функцию renderProductsCards(json)
+ // Аргументом функции является JSON
+ // Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
+ // Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
+ let length = products.length// Запишите в переменную length значение свойства products.length
+ for (let i = 0; i < length; i += 1) {
+     addProduct(products[i]);
+     }
+ }// Напишите цикл, в котором перебираете все products от 0 до (length)
+ // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+ 
